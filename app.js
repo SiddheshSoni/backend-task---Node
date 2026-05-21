e354c28bc820181fff539558bebcd64f9231e286
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-05-21 14:24:58 +0530

    task 2 completed

--- app.js
+++ app.js
@@ -8,20 +8,49 @@ const server = http.createServer((req, res)=>{
 
     if(req.url === "/"){
         res.setHeader('Content-Type', 'text/html');
-        
-        res.end(`
-            <form method="POST" action="/message">
-                <label>Name:</label>
-                <input type='text' name='username'>
-                <button type="submit">Add</button>
-            </form>
-        `);
+
+        fs.readFile('formValues.txt', (err, data)=>{
+            const dataVal = err ? '' : data.toString();
+            console.log(dataVal);
+
+            res.end(`
+                <h1>${dataVal}</h1>
+                <form method="POST" action="/message">
+                    <label>Name:</label>
+                    <input type='text' name='username'>
+                    <button type="submit">Add</button>
+                </form>
+            `);
+        });
     }else{
         if(req.url === "/message"){
             res.setHeader('Content-Type', 'text/html');
-            res.statusCode=302;
-            res.end(`<h1>This is a message</h1>`)
             
+            let dataChunks = [];
+            req.on('data', (chunks)=>{
+                dataChunks.push(chunks);
+            });
+            
+            req.on('end', ()=>{
+                let combinedBuffer = Buffer.concat(dataChunks);
+                let value = combinedBuffer.toString().split("=")[1];
+
+                fs.writeFile('formValues.txt', value, (err)=>{
+                    res.statusCode =302;
+                    res.setHeader('location', '/');
+                    res.end();
+                });
+                
+            })
+        }else{
+            // if(req.url == '/read'){
+            //     fs.readFile('formValues.txt', (err, data)=>{
+            //         console.log(data.toString());
+                    
+                    
+            //         res.end(`<h1>${data.toString()}</h1>`);
+            //     })
+            // }
         }
     }
 
